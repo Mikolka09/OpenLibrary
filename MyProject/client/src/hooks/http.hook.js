dc5cea47aa17d1c92ccad3193b553427db0e55c3
Микола Матіос <mikolka09@gmail.com>
2021-11-19 22:02:22 +0100

    19.11.2021

--- MyProject/client/src/hooks/http.hook.js
+++ MyProject/client/src/hooks/http.hook.js
@@ -16,7 +16,7 @@ export const useHttp = () => {
             const data = await response.json();
 
             if (!response.ok) {
-               throw new Error(data.message || 'Something went wrong!')
+               return new Error(data.message || 'Something went wrong!')
             }
             setLoading(false);
             return data;
